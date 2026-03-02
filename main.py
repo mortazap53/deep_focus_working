863a15df548484428d6f42c22b0c48cc31c9b63d
R1stAMOR <mortazap53@gmail.com>
2026-03-02 14:26:06 +0430

    coding some button and text labels on the screen.

--- main.py
+++ main.py
@@ -29,4 +29,16 @@ canvas.create_image(100, 112, image=tomato_photo)
 canvas.create_text(100, 140, text="00:00", fill="white", font=FONT_NAME)
 canvas.grid(row=2, column=2)
 
+text_Timer = Label(text="Timer", font=("Courier", 30, "bold"), fg=GREEN)
+text_Timer.grid(row=1, column=2)
+
+start_button = Button(text="Start")
+start_button.grid(row=3, column=1)
+
+Reset_button = Button(text="Reset")
+Reset_button.grid(row=3, column=3)
+
+check_box_label = Label(text="✔", font=("Courier", 25, "bold"), fg=GREEN)
+check_box_label.grid(row=4, column=2)
+
 window.mainloop()
