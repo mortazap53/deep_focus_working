558f94d28258a1a556d1ea3fd93f9ebb9d60ec26
R1stAMOR <mortazap53@gmail.com>
2026-03-02 14:04:02 +0430

    making the background ready

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
+from tkinter import *
+
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
-FONT_NAME = "Courier"
+FONT_NAME = ("Courier", 35, "bold")
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Deep Focus Working")
+window.configure(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_photo = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_photo)
+canvas.create_text(100, 140, text="00:00", fill="white", font=FONT_NAME)
+canvas.grid(row=2, column=2)
+
+window.mainloop()
