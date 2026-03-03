7a00153404e2502b3527ca8cf71f6be91503e74f
R1stAMOR <mortazap53@gmail.com>
2026-03-03 09:20:23 +0430

    completing the user interface and the logic of timing and counting. and looping through the work time, short break and long break.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from idlelib.query import Query
 from tkinter import *
 import math
 
@@ -10,12 +11,32 @@ FONT_NAME = ("Courier", 35, "bold")
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+repetition = 0
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def starting_timer():
-    count_down(1 * 60)
+    global repetition
+
+    repetition += 1
+    focusing_time = WORK_MIN * 60
+    short_break_time = SHORT_BREAK_MIN * 60
+    long_break_time = LONG_BREAK_MIN * 60
+
+    if repetition % 8 == 0:
+        count_down(long_break_time)
+        text_mood.config(text="Break", fg=RED)
+    elif repetition % 2 != 0:
+        count_down(focusing_time)
+        text_mood.config(text= "Work", fg=GREEN)
+    elif repetition % 2 == 0:
+        count_down(short_break_time)
+        text_mood.config(text= "Break", fg=PINK)
+    elif repetition > 8:
+        repetition = 1
+    else:
+        pass
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
@@ -27,6 +48,8 @@ def count_down(count):
     canvas.itemconfig(timer, text=f"{count_m}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        starting_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -40,8 +63,8 @@ canvas.create_image(100, 112, image=tomato_photo)
 timer = canvas.create_text(100, 140, text="00:00", fill="white", font=FONT_NAME)
 canvas.grid(row=2, column=2)
 
-text_Timer = Label(text="Timer", font=("Courier", 30, "bold"), fg=GREEN)
-text_Timer.grid(row=1, column=2)
+text_mood = Label(text="Timer", font=("Courier", 30, "bold"), fg=GREEN)
+text_mood.grid(row=1, column=2)
 
 start_button = Button(text="Start", command=starting_timer)
 start_button.grid(row=3, column=1)
